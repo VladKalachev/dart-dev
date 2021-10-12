12570088b1d769505d988d66d1dac784ea6e5677
Corvax <vlvete1990@yandex.ru>
2021-10-12 21:35:23 +0300

    chore: types

--- playground.dart
+++ playground.dart
@@ -1,3 +1,16 @@
+
 main() {
-  print("Hello World");
+  var name = 'Name';
+  String name2 = 'Name2';
+  print("Hello World" + " " + name + " " + name2);
+
+  var age = 30;
+  int age2 = 30;
+
+  print('Age: $age');
+
+  var str = 30.3;
+  double str3 = 30.2;
+
+  print('Double: $str3');
 }
