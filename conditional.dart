df2ffc1218b0a49f11f2dfb20c8a6c4f9d439711
Corvax <vlvete1990@yandex.ru>
2021-10-14 00:13:50 +0300

    chore: switch

--- conditional.dart
+++ conditional.dart
@@ -1,16 +1,28 @@
 // Condotonal Statement
 
 void main() {
-  int number = 100;
+  // int number = 100;
 
-  if (number % 2 == 0) {
-    print('Even');
-  }
-  else if (number % 3 == 0) {
-    print('Odd');
-  }
-  else {
-    print('Confused');
-  }
+  // if (number % 2 == 0) {
+  //   print('Even');
+  // }
+  // else if (number % 3 == 0) {
+  //   print('Odd');
+  // }
+  // else {
+  //   print('Confused');
+  // }
 
+  int number = 0;
+
+  switch (number) {
+    case 0:
+      print('Even');
+      break;
+    case 1:
+      print('Odd');
+      break;
+    default:
+      print('Confused');
+  }
 }
