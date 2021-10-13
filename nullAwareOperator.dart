56dd74139ec1f93b8151e2c7e2fe4ce6b1c6f34b
Corvax <vlvete1990@yandex.ru>
2021-10-13 15:56:03 +0300

    chore: ternary operator

--- nullAwareOperator.dart
+++ nullAwareOperator.dart
@@ -6,13 +6,22 @@ class Num {
 }
 
 main() {
-  var n = Num();
-  // var n;
-  int number;
+  // var n = Num();
+  // // var n;
+  // int number;
 
-  // if (n != null) {
-    number = n?.num ?? 0;
-  // }
+  // // if (n != null) {
+  //   number = n?.num ?? 0;
+  // // }
 
-  print(number);
+  // print(number);
+
+  // int number;
+  // print(number ??= 100);
+  // print(number);
+
+  // Ternary operator
+  int x = 100;
+  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  print(result);
 }
