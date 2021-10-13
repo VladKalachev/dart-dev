a873d4642ab74b023318f7b459ac1dea17cb63f6
Corvax <vlvete1990@yandex.ru>
2021-10-14 00:12:13 +0300

    chore: conditional

---
+++ conditional.dart
@@ -0,0 +1,16 @@
+// Condotonal Statement
+
+void main() {
+  int number = 100;
+
+  if (number % 2 == 0) {
+    print('Even');
+  }
+  else if (number % 3 == 0) {
+    print('Odd');
+  }
+  else {
+    print('Confused');
+  }
+
+}
