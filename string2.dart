0f70ddb1861625b2f1cbc3d28f3ffa29abf261cf
Corvax <vlvete1990@yandex.ru>
2021-10-13 13:28:18 +0300

    chore: string

---
+++ string2.dart
@@ -0,0 +1,17 @@
+// String
+
+main() {
+  var age = 30;
+  var str = 'My age is: $age';
+  print(str);
+
+  var s1 = '''
+  Text
+  ''';
+
+  var s2 = """
+  Text 2
+  """;
+  print(s1);
+  print(s2);
+}
