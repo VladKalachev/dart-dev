f23f04b73a7c252f484293a54e7b3a196089f2ae
Corvax <vlvete1990@yandex.ru>
2021-10-13 13:24:31 +0300

    chore: string

---
+++ string.dart
@@ -0,0 +1,16 @@
+void main() {
+  var s1 = 'Single string';
+  var s2 = "Duble string";
+  var s3 = 'It\'s easy to escape tje sttring delimiter.';
+  var s4 = "It\'s even to escape tje sttring delimiter.";
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print('');
+
+  // RAW String
+  var s = r'In a rew string.';
+  print(s);
+}
