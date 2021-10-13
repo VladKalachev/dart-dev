66b8419cae2496d597193d2434ef513f3536b93a
Corvax <vlvete1990@yandex.ru>
2021-10-13 15:28:40 +0300

    chore: constants

---
+++ constants.dart
@@ -0,0 +1,14 @@
+main() {
+  const aConstNum = 0;
+  const aConstBool = true;
+  const aConstString = 'a constant string';
+ 
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+
+}
