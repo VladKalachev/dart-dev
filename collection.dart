59b1098e54f002c0594a830edaa278c2e4c2e1a7
Corvax <vlvete1990@yandex.ru>
2021-10-15 15:52:00 +0300

    chore: const

--- collection.dart
+++ collection.dart
@@ -2,7 +2,15 @@
 
 void main() {
   // List
-  List names = ['Jack', 'Jill'];
-  print(names[0]);
-  print(names.length);
+  // List names = ['Jack', 'Jill'];
+  // var names = ['Jack', 'Jill', 1, 10.1];
+
+  List<String> names = const ['Jack', 'Jill'];
+
+  // print(names[0]);
+  // print(names.length);
+  // names[1] = 'Mark';
+  for (var n in names) {
+    print(n);
+  }
 }
