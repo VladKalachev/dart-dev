b512a9464aa0a10479f55e6fd62717a94ce79844
Corvax <vlvete1990@yandex.ru>
2021-10-15 15:43:04 +0300

    chore: continue

--- break.dart
+++ break.dart
@@ -3,6 +3,10 @@
 void main() {
   for (var i = 0; i < 10; i++) {
     if (i > 5) break;
-    print(i);
+    // print(i);
+  }
+  for (var i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    print("Odd: $i");
   }
 }
