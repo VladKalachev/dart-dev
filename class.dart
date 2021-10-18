fa1668728828ee510f22b75346e0892b9a724f9c
Corvax <vlvete1990@yandex.ru>
2021-10-18 22:36:53 +0300

    chore: extends

--- class.dart
+++ class.dart
@@ -1,60 +1,32 @@
 // Class
 
-class Person {
-  late String name;
-  late int age;
+class Vehicle {
+  String model;
+  int year;
 
-  // Person(String name, [int age = 18]) {
-  //   this.name = name;
-  //   this.age = age;
-  // }
-  Person(this.name, [this.age = 18]);
-
-  // named constuctor
-  Person.guest() {
-    name = 'Guest';
-    age = 14;
+  Vehicle(this.model, this.year) {
+    print(this.model);
+    print(this.year);
   }
 
   void showOutput() {
-    print(name);
-    print(age);
+    print(model);
+    print(year);
   }
 }
 
-class X {
-  final name; // type will be defined by inferred value
-  static const int age = 10;
+class Car extends Vehicle {
+  double price;
 
-  X(this.name);
+  Car(String model, int year, this.price) : super(model, year);
+
+  void showOutput() {
+    super.showOutput();
+    print(this.price);
+  }
 }
 
 void main() {
-  final name = 'Mab';
-  const age = 30;
-
-  print(name);
-  print(age);
-
-  name = '';
-  age = 0;
-  // Person person1 = Person('Name');
-  // // person1.age = 30;
-  // // person1.name = 'Name';
-  // person1.showOutput();
-
-  // Person person2 = Person('Jack', 25);
-  // person2.showOutput();
-
-  // var person3 = Person.guest();
-  // person3.showOutput();
-  var x = X('Name');
-  print(x.name);
-
-  // x.name = 'new';
-  // print(x.name);
-  // print(X.age);
-
-  var y = X('jill');
-  print(y.age);
+  var car1 = Car('Accord', 2020, 1500000);
+  car1.showOutput();
 }
