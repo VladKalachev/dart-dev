18ff43787bae0283c578aac3ef6e5988e37b91bd
Corvax <vlvete1990@yandex.ru>
2021-10-18 22:52:47 +0300

    chore: Exception Handling

---
+++ exception.dart
@@ -0,0 +1,31 @@
+// Exception Handling
+
+int mustGreaterThanZero(int val) {
+  if (val <= 0) {
+    throw Exception('Value must be greater than zero');
+  }
+  return val;
+}
+
+void letVerifyTheValue(var val) {
+  var valueVerification;
+
+  try {
+    valueVerification = mustGreaterThanZero(val);
+  }
+  catch(e) {
+    print(e);
+  }
+  finally {
+    if (valueVerification == null) {
+      print('Value is not accepted');
+    }
+    else {
+      print('Value varified: $valueVerification');
+    }
+  }
+}
+
+void main() {
+  letVerifyTheValue(0);
+}
