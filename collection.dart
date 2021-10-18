2f42c1a172a50fb3cd5ef294870eaea67ad93777
Corvax <vlvete1990@yandex.ru>
2021-10-18 21:51:02 +0300

    chore: link changed collections

--- collection.dart
+++ collection.dart
@@ -5,12 +5,18 @@ void main() {
   // List names = ['Jack', 'Jill'];
   // var names = ['Jack', 'Jill', 1, 10.1];
 
-  List<String> names = const ['Jack', 'Jill'];
+  // List<String> names = const ['Jack', 'Jill'];
+  List<String> names = ['Jack', 'Jill'];
+
+
+  var names2 = [...names];
+
+  names[1] = 'Mark';
 
   // print(names[0]);
   // print(names.length);
   // names[1] = 'Mark';
-  for (var n in names) {
+  for (var n in names2) {
     print(n);
   }
 }
