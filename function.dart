5cd7c051eb67ea07253dbc20efa4dd103cffb364
Corvax <vlvete1990@yandex.ru>
2021-10-18 22:06:44 +0300

    chore: arrow function

--- function.dart
+++ function.dart
@@ -2,10 +2,18 @@
 
 void main() {
   // Arrow Function =>
-  showOutput(square(2));
-  showOutput(square(2.3));
+  // showOutput(square(2));
+  // showOutput(square(2.3));
 
-  showOutput(sum(1, 2.3));
+  // showOutput(sum(1, 2.3));
+
+  var list = ['apples', 'banans', 'oranges'];
+
+  list.forEach(printF);
+}
+
+void printF(item) {
+  print(item);
 }
 
 dynamic sum(var a,var b) => a + b;
