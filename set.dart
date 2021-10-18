f60f8d3e2e821a1b052b87570703ef0e2e49933f
Corvax <vlvete1990@yandex.ru>
2021-10-18 21:54:41 +0300

    chore: Set

---
+++ set.dart
@@ -0,0 +1,13 @@
+// Collection
+
+void main() {
+  // Set
+  var halogens = <String>{'fluorine', 'chlorine'};
+
+  for (var x in halogens) {
+    print(x);
+  }
+
+  print(halogens.runtimeType);
+
+}
