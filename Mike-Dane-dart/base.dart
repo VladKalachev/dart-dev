26a0cf2b79b3d89d679756b561c06e8bfa1f5b5e
Corvax <vlvete1990@yandex.ru>
2021-12-01 22:08:06 +0300

    choreL length

--- Mike-Dane-dart/base.dart
+++ Mike-Dane-dart/base.dart
@@ -1,17 +1,23 @@
 import 'dart:ffi';
 
 void main() {
-  String name = "Oscar";
-  // String age = "60";
+  // String name = "Oscar";
+  // // String age = "60";
 
-  print("There once was a nab baned ${name}");
-  // print("Hello World");
+  // print("There once was a nab baned ${name}");
+  // // print("Hello World");
 
-  String firstName = "Mike";
-  int age = 26;
-  double gpa = 3.5;
-  bool isRegisteredVoter = false;
-  dynamic name2 = "Name";
+  // String firstName = "Mike";
+  // int age = 26;
+  // double gpa = 3.5;
+  // bool isRegisteredVoter = false;
+  // dynamic name2 = "Name";
 
+  String greeting = "Hello";
+  // 01234
+  
+  print(
+    greeting.length
+  );
 
 }
