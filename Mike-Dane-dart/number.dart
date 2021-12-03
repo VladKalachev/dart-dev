c1be46d76b29a032d5a43c46605286e3842d4958
Corvax <vlvete1990@yandex.ru>
2021-12-03 11:10:54 +0300

    chore: numbers

--- Mike-Dane-dart/number.dart
+++ Mike-Dane-dart/number.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 void main() {
   
-  int quantity = 2;
+  int quantity = 300;
   double price = 5.99;
 
-  print( 5 + 3 );
+  // quantity = quantity + 50;
+  // quantity += 50;
+  // quantity ++;
+
+
+  print(
+    min(1, 2)
+  );
 
 
 }
