062fcfa6c9a18fe0840ac0fd2139365557089b19
Corvax <vlvete1990@yandex.ru>
2021-12-06 12:20:53 +0300

    chore: arguments

--- Mike-Dane-dart/function.dart
+++ Mike-Dane-dart/function.dart
@@ -1,7 +1,12 @@
-int sum(a, b) {
+int sum(int a, int b) {
   return a + b;
+}
+
+void sayHi(String name) {
+  print("Hi ${name} !");
 }
 
 void main(List<String> args) {
   print(sum(1, 2));
+  sayHi("Mike");
 }
