6ea55aa8584293cfb29dd9481207533b32ad8250
Corvax <vlvete1990@yandex.ru>
2021-12-06 12:50:44 +0300

    chore: statements

---
+++ Mike-Dane-dart/statements.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  
+  bool isSmart = true;
+  bool isStudent = true;
+
+  if(isSmart && isStudent) {
+    print("It's true");
+  } else {
+    print("It's false");
+  }
+}
