dfc71fd4f868c2e33e2ce8abf8bf68533ca02a0d
Corvax <vlvete1990@yandex.ru>
2021-12-07 12:07:39 +0300

    chore: for loop

---
+++ Mike-Dane-dart/for-loops.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  
+  List<String> friends = ["Stanley", "Pam", "Karen"];
+
+  print(friends);
+
+  for(int i = 0; i < friends.length; i++) {
+    print(
+      friends[i].toUpperCase()
+    );
+  }
+
+}
