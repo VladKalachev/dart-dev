f5aa66ca586cb967aa829b6374131c42d55a7d7e
Corvax <vlvete1990@yandex.ru>
2021-12-07 11:29:42 +0300

    chore: else if

--- Mike-Dane-dart/statements.dart
+++ Mike-Dane-dart/statements.dart
@@ -1,9 +1,15 @@
 void main(List<String> args) {
   
   bool isSmart = true;
-  bool isStudent = true;
+  bool isStudent = false;
 
   if(isSmart && isStudent) {
+    print("It's true");
+  } else if (isSmart && !isStudent) {
+    print("It's false");
+  }
+
+  if(isSmart || isStudent) {
     print("It's true");
   } else {
     print("It's false");
