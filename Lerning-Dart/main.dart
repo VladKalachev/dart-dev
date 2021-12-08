128db4cdbce64a4c60e7c55f7dad7c27288c3640
Corvax <vlvete1990@yandex.ru>
2021-12-08 12:17:15 +0300

    chore: final and const

--- Lerning-Dart/main.dart
+++ Lerning-Dart/main.dart
@@ -5,4 +5,12 @@
 void main(List<String> args) {
   // Простой комментарий
   print("Welcome to Dart");
+
+  var num = 1;
+  print(num);
+
+  const pi = 3.14;
+  final String name = "Konstantin";
+  
+
 }
