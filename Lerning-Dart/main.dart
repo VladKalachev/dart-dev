c3721191b59b2f7de23185b4d6febc74664747c9
Corvax <vlvete1990@yandex.ru>
2021-12-08 12:31:17 +0300

    chore: base type

--- Lerning-Dart/main.dart
+++ Lerning-Dart/main.dart
@@ -12,5 +12,38 @@ void main(List<String> args) {
   const pi = 3.14;
   final String name = "Konstantin";
   
+  // Number (int, double)
+  double my_weight = 84.5;
+  print(my_weight.runtimeType);
+
+  // Boolean
+  bool isChildren = true;
+  print(isChildren.runtimeType);
+
+  // String
+  String my_name = "Vlad";
+  String my_surname = "Kalachev";
+  print(my_name.runtimeType);
+  String test = 'What\'s your name?';
+  print(test);
+  String fio = '''
+    Name
+    Surname
+    Age 
+  ''';
+  print(fio);
+
+  String person_info = "Name: ${my_name.toUpperCase()}, Surname: $my_surname";
+  print(person_info);
+
+  // Runes (UTF-32)
+  Runes hello = Runes('\u041F');
+  print(hello);
+  print(String.fromCharCodes(hello));
+
+  // Symbol
+  Symbol s = #f;
+  print(s);
+
 
 }
