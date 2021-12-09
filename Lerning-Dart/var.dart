59b69c7cf6bccb5574ca1f6df4719b0e484e6951
Corvax <vlvete1990@yandex.ru>
2021-12-09 12:42:59 +0300

    chore: dynamic and var

---
+++ Lerning-Dart/var.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  dynamic message = "Some text";
+  print(message.runtimeType);
+  message = 4;
+  print(message.runtimeType);
+  var number = 10;
+  print(number.runtimeType);
+
+
+}
