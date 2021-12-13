76f6e0507e8b73e260f9fac63f03b1cba95de8c8
Corvax <vlvete1990@yandex.ru>
2021-12-13 13:20:29 +0300

    chore: insaid function

--- Lerning-Dart/functionThree.dart
+++ Lerning-Dart/functionThree.dart
@@ -1,4 +1,9 @@
+
+String topLevelVariable = "Top Level Variable";
+
 void main(List<String> args) {
+
+  String variableInsideMain = "Variable Inside Main";
   // Function addTwoNumbers = (int a, int b) {
   //   var sum = a + b;
   //   print(sum);
@@ -9,6 +14,24 @@ void main(List<String> args) {
   addTwoNumbers(1, 2);
   int result = operationNumbers(5, 2, (a, b) => a - b);
   print(result);
+
+  void myFunction() {
+    String variableInsideMyFunction = "Variable Inside My Function";
+    print("Function");
+
+    void nestedFunction() {
+      String variableInsideNestedFunction = "Variable Inside Nested Function";
+      print(topLevelVariable);
+      print(variableInsideMain);
+      print(variableInsideMyFunction);
+      print(variableInsideNestedFunction);
+      print("Inside Nested Function");
+    }
+
+    nestedFunction();
+  }
+
+  myFunction();
 }
 
 int operationNumbers(int a, int b, Function operation) {
