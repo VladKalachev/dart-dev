11ec3d553f3378e3a4f88e5a410e1126b4e796f0
Corvax <vlvete1990@yandex.ru>
2021-12-13 10:58:36 +0300

    chore: array function

--- Lerning-Dart/function.dart
+++ Lerning-Dart/function.dart
@@ -2,12 +2,24 @@ void main(List<String> args) {
   sayHello();
   String myName = getName();
   print(myName);
+  String myAge = getAge(20);
+  print(myAge);
 }
 
-void sayHello() {
-  print("Hello");
-}
+void sayHello() => print("Hello");
 
-String getName() {
-  return "Vlad";
-}
+String getName() => "Vlad";
+
+String getAge(int age) => "Your age $age";
+
+// void sayHello() {
+//   print("Hello");
+// }
+
+// String getName() {
+//   return "Vlad";
+// }
+
+// String getAge(int age) {
+//   return "Your age $age";
+// }
