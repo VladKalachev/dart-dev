46ceeb6ecb934fb64455426f705d8a0a6b56ebcc
Corvax <vlvete1990@yandex.ru>
2021-12-16 12:00:50 +0300

    chore: stack trace

--- Lerning-Dart/exception.dart
+++ Lerning-Dart/exception.dart
@@ -3,11 +3,14 @@ void main(List<String> args) {
   // Error
   try {
     var list = [double.parse('3,14'), 2, 3];
-    print(list);
-  } catch (e) {
-    // Error
+    print("Result: ${list[5]}");
+  } on RangeError {
+    print("Out of bound of list");
+  } catch (e, s) {
+    print("Stack Trace: $s");
     print("The Exeption: $e");
+  } finally {
+    print("This is finally and is always executed");
   }
-  
-  
+
 }
