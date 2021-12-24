89da408fd6b03d5eeb33a312d18be0cb91cc5902
Corvax <vlvete1990@yandex.ru>
2021-12-24 19:00:05 +0300

    chore: named constructor

--- Lerning-Dart/class.dart
+++ Lerning-Dart/class.dart
@@ -1,7 +1,12 @@
 void main(List<String> args) {
-  Car myCar = Car("BMW", color: "White");
+  Car myCar = Car.named();
   myCar.start();
 
+  Car myCar2 = Car("BMW", color: "White");
+  myCar2.start();
+
+  Car myCar3 = Car.fromColor("ORANGE");
+  myCar3.start();
 
   // myCar.carName = "BMW";
   // myCar.color = 0xFFF;
@@ -24,6 +29,17 @@ class Car {
   //   this.color = color;
   // }
   // Car(this.carName, [this.color = "Red"]);
+
+  Car.named() {
+    carName = "FORD";
+    color = "GREEN";
+  }
+
+  Car.fromColor(var color) {
+    carName = "RED";
+    this.color = color;
+  }
+
   Car(this.carName, {this.color = "Red"});
 
   void start() {
