3826736308aaefed872496d45b823820c0b583c4
Corvax <vlvete1990@yandex.ru>
2022-01-26 21:13:19 +0300

    chore: class constrants

--- Lerning-Dart/classConstants.dart
+++ Lerning-Dart/classConstants.dart
@@ -1,14 +1,19 @@
 void main(List<String> args) {
-  Car car = Car("BMW", 45);
+  Car car = Car(2, "BMW", 45);
   car.move();
+  print(car.wheels);
 }
 
 class Car {
   String carName;
   double percenyWay;
-  // int wheels; // Число колес у автомобиля
+  final int wheels; // Число колес у автомобиля
 
-  Car(this.carName, this.percenyWay);
+  // Car(this.wheels, this.carName, this.percenyWay);
+  Car(wheels, carName, percenyWay): wheels = wheels + 2 {
+    this.carName = carName;
+    this.percenyWay = percenyWay;
+  }
 
   void move() {
     print("The $carName has started. Percent way: $percenyWay");
