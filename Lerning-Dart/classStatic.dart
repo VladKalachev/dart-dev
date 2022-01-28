ca83a9448e1a0985a15feaa1cb6bef581bbf562c
Corvax <vlvete1990@yandex.ru>
2022-01-28 19:34:05 +0300

    chore: static class

--- Lerning-Dart/classStatic.dart
+++ Lerning-Dart/classStatic.dart
@@ -1,14 +1,29 @@
 void main(List<String> args) {
   Car car = Car("BMW", "Red");
+  // Car.wheels = 4;
+  Car.setWheels(3);
   car.checkMove();
 }
 
 class Car {
   String carName;
   String color;
-  static int wheels = 3;
+  static int wheels = 4; // или consts
 
-  Car(this.carName, this.color);
+  Car(this.carName, this.color) {
+    var t = time(500, 80);
+    print("Time: $t");
+  }
+
+  static double time(int dist, double speed) => dist / speed;
+
+  static void setWheels(int value) {
+    if (value == 4) {
+      wheels = value;
+    } else {
+      print("Please, correct value (4)");
+    }
+  }
 
   void checkMove() {
     if (wheels < 4) {
