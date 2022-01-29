4592ccaf1e182613d0a8283d79cf34328fc6a732
Corvax <vlvete1990@yandex.ru>
2022-01-29 19:06:40 +0300

    chore: extends class

--- Mike-Dane-dart/extends.dart
+++ Mike-Dane-dart/extends.dart
@@ -1,13 +1,19 @@
 void main(List<String> args) {
-  var myCar = Car();
-  myCar.carName = "BMW";
-  myCar.color = "Red";
+  var myCar = Car("BMW", 'Red');
   myCar.start();
-  myCar.open();
+  // myCar.carName = "BMW";
+  // myCar.color = "Red";
+  // myCar.start();
+  // myCar.open();
 }
 
 class Vehicle {
   String color = '';
+
+  Vehicle.fromColor(var color) {
+    this.color = color;
+    print("Call Vehicle.fromColor: $color Constructor");
+  }
 
   void start() {
     print("Has started");
@@ -16,6 +22,11 @@ class Vehicle {
 
 class Car extends Vehicle {
   String carName = '';
+
+  Car(String carName, String color): super.fromColor(color) {
+    this.carName = carName;
+    print("Call Car.Name: $carName, Color: $color");
+  }
 
   void open() {
     print("Open doors");
