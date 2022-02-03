98a9593b2ead899071e9a408b09cf999b40b83d9
Corvax <vlvete1990@yandex.ru>
2022-02-03 21:29:21 +0300

    chore: generic

--- Mike-Dane-dart/generic.dart
+++ Mike-Dane-dart/generic.dart
@@ -1,21 +1,23 @@
 void main(List<String> args) {
   Car car = Car("1", "BMW");
   print(car.id.runtimeType);
+  car.move("100");
 
    Car car2 = Car(1, "BMW");
-  print(car2.id.runtimeType);
+  // print(car2.id.runtimeType);
+  car2.move(100);
 
   // int id = car.id;
   // print(id);
 }
 
-class Car<T> {
+class Car<T> { 
   T id;
   String carName;
 
   Car(this.id, this.carName);
 
-  void move() {
-    print("The $carName. ID number: $id");
+  void move<T>(T speed) {
+    print("The $carName. ID number: $id. Speed $speed");
   }
 }
