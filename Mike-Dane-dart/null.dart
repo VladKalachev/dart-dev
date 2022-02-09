dce9d88fe58acd234ae177b3ec0beab62fc13704
Corvax <vlvete1990@yandex.ru>
2022-02-09 16:10:21 +0300

    chore: value is not defiend

--- Mike-Dane-dart/null.dart
+++ Mike-Dane-dart/null.dart
@@ -3,4 +3,25 @@ void main(List<String> args) {
   String name = 'Mart'; // not nullable
   print(name);
   print(age);
+  print(someValue(25));
+  print(someValue(null));
+
+  int x;
+  if(25 > 0) {
+    x = 1;
+  } else {
+    x = -1;
+  }
+  print(x);
+}
+
+int someValue(int? value) {
+  if(value == null) {
+    return 0;
+  }
+  return value;
+}
+
+Never valueIsNotDefiend() {
+  throw ArgumentError('Value is not defined');
 }
