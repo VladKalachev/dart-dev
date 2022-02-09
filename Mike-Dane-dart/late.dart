08631bbfc3659ce7287c81485f02828f4646627a
Corvax <vlvete1990@yandex.ru>
2022-02-09 16:24:55 +0300

    chore: late

---
+++ Mike-Dane-dart/late.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  var pizza = Pizza();
+  print(pizza.printPizza());
+}
+
+class Pizza {
+  // int id = 1;
+  // String name = 'Spring';
+  // double price = 10;
+  int id;
+  String name;
+  double price;
+
+  Pizza() : id = 1, name = 'Spring', price = 10;
+
+  String printPizza() {
+    print('Cooking pizza...');
+    return 'Pizza ($id): $name, price: $price';
+  }
+}
