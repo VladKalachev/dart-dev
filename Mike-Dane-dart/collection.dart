f86c213cc18e13bd046ba9e82cbdd78dd1deac9a
Corvax <vlvete1990@yandex.ru>
2022-02-11 23:38:35 +0300

    chore: collection Map

--- Mike-Dane-dart/collection.dart
+++ Mike-Dane-dart/collection.dart
@@ -3,15 +3,37 @@
 // ?[]
 
 void main(List<String> args) {
-  List<String?> nullableValues = [null, 'London', null, 'Moscow', 'NY'];
-  List<String>? nullableList;
-  List<String?>? nullableListAndNullableValue = [null, 'String'];
+  // List<String?> nullableValues = [null, 'London', null, 'Moscow', 'NY'];
+  // List<String>? nullableList;
+  // List<String?>? nullableListAndNullableValue = [null, 'String'];
 
-  print(nullableList?[0]);
+  // print(nullableList?[0]);
 
-  nullableValues[0] ??= 'SPB';
-  print(nullableValues);
+  // nullableValues[0] ??= 'SPB';
+  // print(nullableValues);
 
-  String someString = nullableValues[2] ?? 'Vologda';
-  print(someString);
+  // String someString = nullableValues[2] ?? 'Vologda';
+  // print(someString);
+
+  Map<String, int> categories = { 'Kitchen': 1, 'Bedroom': 2 };
+  Map<String, int>? nullableMap;
+  Map<String, int?> nullableValues = { 'null': null };
+  Map<String, int?>? nullableMapAndNullableValue;
+
+  print(categories['Room']);
+
+  int value1 = categories['Room'] ?? 3;
+  print(value1);
+
+  int value2 = categories['Room'] ??= 3;
+  print(value2);
+  print(categories);
+
+  int? value3 = categories['Kitchen'];
+  print(value3);
+  print(categories);
+
+  int value4 = categories['Kitcehb']!;
+  print(value4);
+  print(categories);
 }
