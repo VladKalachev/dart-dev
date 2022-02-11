42838ed126ff854a1326145febc5120b2fd11309
Corvax <vlvete1990@yandex.ru>
2022-02-11 23:48:49 +0300

    chore: class

---
+++ Mike-Dane-dart/class2.dart
@@ -0,0 +1,26 @@
+// Sound Null Safety
+
+// Position mandatory = f(int x)
+// Position optional = f([int x])
+// Named optional = f({int x})
+// Named mandatory = ???
+
+void main(List<String> args) {
+  // var pizza = Pizza(1, 'Spring');
+  var pizza = Pizza();
+  print(pizza.printPizza());
+}
+
+class Pizza {
+  final int? id;
+  final String? name;
+  final double? price;
+
+  // Pizza(this.id, this.name, [this.price = 10]);
+   Pizza({this.id, this.name, this.price});
+
+  String printPizza() {
+    print('Coocking pizza...');
+    return 'Pizza ($id): $name, price: $price';
+  }
+}
